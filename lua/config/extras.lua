af306135d5bef422a885ca833866a5b09b473e6f
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-09-30 22:14:36 +0100

    Add new function
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- lua/config/extras.lua
+++ lua/config/extras.lua
@@ -1,13 +1,20 @@
-vim.api.nvim_create_user_command('Wttr', function(opts)
-	vim.cmd('new')
-	vim.cmd('term curl wttr.in/London')
+vim.api.nvim_create_user_command("Wttr", function(opts)
+  vim.cmd("new")
+  vim.cmd("term curl wttr.in/London")
 end, {
-	nargs = 0,
+  nargs = 0,
 })
 
-vim.api.nvim_create_user_command('StarWars', function(opts)
-	vim.cmd('new')
-	vim.cmd('term telnet towel.blinkenlights.nl')
+vim.api.nvim_create_user_command("StarWars", function(opts)
+  vim.cmd("new")
+  vim.cmd("term telnet towel.blinkenlights.nl")
 end, {
-	nargs = 0,
+  nargs = 0,
+})
+
+vim.api.nvim_create_user_command("X4248pull", function(opts)
+  vim.cmd("!cd ~/.config/nvim && git pull")
+  vim.cmd('echo "4248 Config updated. Please restart neovim."')
+end, {
+  nargs = 0,
 })
